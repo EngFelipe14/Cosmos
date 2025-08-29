8468cead62f549b206eae4bd865b1bee9b6cd131
felipe <felipefulldev14@gmail.com>
2025-08-29 01:04:07 -0500

    feat: Se creo el servidor y conexión a la base de datos
    chore: instalar @types/express y @types/pg para soporte de tipos
    
    fix: corregir importación de archivos .ts mediante configuración en tsconfig.json debido a errores al solo permitir extensiones .js
    - Se añadió allowImportingTsExtensions y emitDeclarationOnly
    
    chore: Se modificó levemente los scripts dev y build en package.json"

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Configuración del espacio de trabajo backend realizada")
+import express from "express";
+import type { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config({ path: '../.env' });
+const app: Application = express();
+
+app.use(express.json());
+
+app.get("/prueba", (request: Request, response: Response) => {
+    response.json({message: "prueba exitosa"})
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (): void => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
